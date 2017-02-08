f58b61756dea2aa6bf68d48c07040bbe00e08fbb
lim-hanbin <lim.hanbin@gmail.com>
2017-02-08 15:32:06 +0800

    Version 11 upload.

---
+++ js/pw.js
@@ -0,0 +1,65 @@
+// ----------------------------------------------------------------------------------
+// |                               Paginas Mayores                                  |
+// ----------------------------------------------------------------------------------
+function pw_deberes() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/sh/fjlqxi2cjodcnq4/AACAgEKKzerM95uVqcjq4v5aa";
+  }
+}
+function pw_curso() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://lim-hanbin.github.io/kopicalentito/curso.html";
+  }
+}
+
+// ----------------------------------------------------------------------------------
+// |                              Libros para niños                                 |
+// ----------------------------------------------------------------------------------
+function pw_nino01() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/f4ha7sqs36m74y8/Gol%20de%20Federico.pdf";
+  }
+}
+function pw_nino02() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/l2z2hptn8937prt/Tairon%20el%20horrible.pdf";
+  }
+}
+function pw_nino03() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/f0yaaofu4ae1bzs/Tairon%20el%20super%20tramposo.pdf";
+  }
+}
+function pw_nino04() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/dyel6o7f17m8chp/Tairon%20en%20puros.pdf";
+  }
+}
+function pw_nino05() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/ghivz2zgboujqmg/Que%20desastre.pdf";
+  }
+}
+
+// ----------------------------------------------------------------------------------
+// |                                 Otros Libros                                   |
+// ----------------------------------------------------------------------------------
+function pw_otro01() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/618u8kyyb06g04g/Spanish%20Verbs%20and%20Essentials%20of%20Grammar.pdf";
+  }
+}
+function pw_otro02() {
+  var nombre = prompt("¡Contraseña! ¿Qué es el nombre de su profesor?", "");
+  if (nombre == "Marisa" || nombre == "marisa") {
+    window.location = "https://www.dropbox.com/s/72q2k12p73a3ey6/Spanish%20Word%20Games%20For%20Dummies%202010.pdf";
+  }
+}
